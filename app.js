a0f50845fbd96c0f5994395d4b1e6f4cc7235465
UsopKing7 <programadorusop@gmail.com>
2025-02-28 05:06:40 -0400

    mejorando la app principal

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 import express from 'express';
-import router from './routers/controllers.js';
+import routes from './routers/index.js';
 
 const app = express()
 app.use(express.json())
 
 // los Gets dando informcion al usuaior 
-app.use('/movies', router)
-app.use('/movies/:id', router)
+
+routes.forEach(routes => app.use('/movies', routes))
 
 // servidor escuchando en el puerto
 const port = process.env.PORT ?? 3333
 app.listen(port, () => {
     console.log('Servidor => http://localhost:' + port)
 })
+
 
