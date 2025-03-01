07ecaae777c9bad1c31663cae7ab79a0f8786927
UsopKing7 <programadorusop@gmail.com>
2025-03-01 16:03:00 -0400

    mejorando la salida de consola

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 import express from "express";
 import routes from "./src/controllers/index.js";
 import fsp from "fs/promises";
+import pc from 'picocolors';
 
 const app = express();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
   try {
-    console.log('peticion recivida...', req.url)
+    console.log(pc.blue('peticion recivida...'), req.url)
     const data = await fsp.readFile("./src/views/index.html", "utf-8");
     res.status(200).send(data);
   } catch (error) {
@@ -22,7 +23,7 @@ app.use('/src/views', express.static("src/views"));
 routes.forEach((routes) => app.use("/movies", routes));
 
 app.use((req, res) => {
-    console.log('peticion recivida...', req.url)
+    console.log(pc.red('peticion recivida...'), req.url)
     res.status(404).json({ message: "Error 404 - Not Found" });
   });
   
@@ -30,5 +31,7 @@ app.use((req, res) => {
 // servidor escuchando en el puerto
 const port = process.env.PORT ?? 3333;
 app.listen(port, "0.0.0.0", () => {
-  console.log("Servidor => http://0.0.0.0:" + port);
+  console.table({
+    url: 'http://localhost:' + port
+  })
 });
